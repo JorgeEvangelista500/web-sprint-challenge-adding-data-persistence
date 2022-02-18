2f9a182c292ee3a2c6157a7c7017c9c0a5b0a13a
Jorge <jorgeevangelista500@gmail.com>
2022-02-18 10:18:45 -0500

    added routers

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,11 @@
 // build your `/api/projects` router here
+const router = require('express').Router()
+const Project = require('./model')
+
+// router.get('/', (req, res, next) => {
+//     next()
+// }
+    
+
+
+module.exports = router
