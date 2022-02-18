eac77dfd8d8c7695cb49491fa2a62dc92070afd9
Jorge <jorgeevangelista500@gmail.com>
2022-02-18 11:52:02 -0500

    get all resources

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,11 @@
+const db = require('../../data/dbConfig')
+
 // build your `Resource` model here
+
+function getResources() {
+    return db('resources')
+}
+
+module.exports = {
+    getResources,
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -2,5 +2,13 @@
 const router = require('express').Router()
 const Resource = require('./model')
 
+router.get('/', (req, res, next) => {
+    Resource.getResources()
+        .then(resources => {
+            res.status(200).json(resources)
+        })
+        .catch(next)
+})
+
 
 module.exports = router
